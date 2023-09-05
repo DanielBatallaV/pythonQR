b3f587a79f1c89d7f02949269a5e6700baaf920a
DanielBatallaV <daniel.batalla@usach.cl>
2023-09-05 23:58:53 +0200

    Update main

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import qrcode
 
-# Enlace del archivo PDF que obtuviste de Google Drive PDF
-# url = "https://drive.google.com/file/d/1tbdCNCBIetWjrmEzQQ1ySujFYQV-7eYS/view"
 # Enlace del archivo PDF que obtuviste de Google Drive carpeta
-url = "https://drive.google.com/drive/folders/1sS4j9IIhr7jWranIcGdKBTkW9WCYgB3T"
-# Generar el código QR
-img = qrcode.make(url)
+url_1 = "https://drive.google.com/drive/folders/1NFzirJinjdW83Wl36llKHH-K-2l2FsGX?usp=sharing"
+url_2 = "https://drive.google.com/file/d/1lyRaviLzbtHpVjeC_LcSOcKzSi9GFa5o/view?usp=sharing"
 
-# Guardar el código QR en un archivo
-img.save("qrImages/Folder_menu_qr.png")
+urls = [
+    { 'name': 'opciones_sazon', 'link': url_1  },
+    { 'name': 'menu_sazon', 'link': url_2  }
+    ]
+
+for url_item in urls:
+    # Generar el código QR para el enlace
+    img = qrcode.make(url_item['link'])
+
+    # Guardar el código QR en un archivo usando el nombre del enlace
+    filename = "qrImages/{}.png".format(url_item['name'])
+    img.save(filename)
